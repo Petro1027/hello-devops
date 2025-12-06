b34c04baeeb87a96dc321ea9d150f943e79282d0
Petro <gergo.petrovszky@thewildgentlemen.com>
2025-12-06 10:00:58 +0100

    Initial commit: basic .NET 8 Hello DevOps app

---
+++ HelloDevOps/Program.cs
@@ -0,0 +1,9 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello DevOps from .NET 8! 🚀");
+
+app.Run();
